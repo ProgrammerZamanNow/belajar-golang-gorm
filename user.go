69fe8bad63b72a6561eb8f97d5e8ea639161d3aa
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-10-27 21:38:29 +0700

    field permission

--- user.go
+++ user.go
@@ -3,11 +3,12 @@ package belajar_golang_gorm
 import "time"
 
 type User struct {
-	ID        string    `gorm:"primary_key;column:id"`
-	Password  string    `gorm:"column:password"`
-	Name      string    `gorm:"column:name"`
-	CreatedAt time.Time `gorm:"column:created_at;autoCreateTime"`
-	UpdatedAt time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
+	ID          string    `gorm:"primary_key;column:id;<-:create"`
+	Password    string    `gorm:"column:password"`
+	Name        string    `gorm:"column:name"`
+	CreatedAt   time.Time `gorm:"column:created_at;autoCreateTime;<-:create"`
+	UpdatedAt   time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
+	Information string    `gorm:"-"`
 }
 
 func (u *User) TableName() string {
