3e8bfd343a60c7da4e35a4bbb890c11c3a5cd655
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-10-27 21:43:25 +0700

    embedded struct

--- database.sql
+++ database.sql
@@ -18,4 +18,16 @@ create table users
     primary key (id)
 ) engine = InnoDB;
 
+select *
+from users;
+
+alter table users
+    rename column name to first_name;
+
+alter table users
+    add column middle_name varchar(100) null after first_name;
+
+alter table users
+    add column last_name varchar(100) null after middle_name;
+
 select * from users;
--- user.go
+++ user.go
@@ -5,7 +5,7 @@ import "time"
 type User struct {
 	ID          string    `gorm:"primary_key;column:id;<-:create"`
 	Password    string    `gorm:"column:password"`
-	Name        string    `gorm:"column:name"`
+	Name        Name      `gorm:"embedded"`
 	CreatedAt   time.Time `gorm:"column:created_at;autoCreateTime;<-:create"`
 	UpdatedAt   time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
 	Information string    `gorm:"-"`
@@ -13,4 +13,10 @@ type User struct {
 
 func (u *User) TableName() string {
 	return "users"
+}
+
+type Name struct {
+	FirstName  string `gorm:"column:first_name"`
+	MiddleName string `gorm:"column:middle_name"`
+	LastName   string `gorm:"column:last_name"`
 }
